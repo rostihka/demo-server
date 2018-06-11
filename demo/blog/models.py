93335f9b824586b3052747f291e3677dad959865
asukhrin <asukhrin@kmilearning.com>
2018-06-12 02:05:41 +0300

    created app accounts

--- demo/blog/models.py
+++ demo/blog/models.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.conf import settings
 from django.utils.six import python_2_unicode_compatible
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Base(models.Model):
+    class Meta:
+        abstract = True
+
     created = models.DateTimeField(editable=False)
     updated = models.DateTimeField()
 
@@ -19,7 +23,18 @@ class Base(models.Model):
 
 
 @python_2_unicode_compatible
-class Category(models.Model):
+class Category(Base):
+    name = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', kwargs={'category_name': self.slug})
+
+    def __str__(self):
+        return self.name[:20]
+
+
+@python_2_unicode_compatible
+class Tag(Base):
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -27,25 +42,26 @@ class Category(models.Model):
 
 
 @python_2_unicode_compatible
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return  self.name[:20]
-
-
-@python_2_unicode_compatible
 class Article(Base):
     class Meta:
         ordering = ['-created']
-    title = models.CharField(max_length=50)
-    body = models.TextField()
-    image = models.ImageField(upload_to='img/blog')
-    views = models.PositiveIntegerField(default=0)
 
-    category = models.ForeignKey(Category)
+    STATUS_CHOICES = (
+        ('d', 'part'),
+        ('p', 'publised')
+    )
+
+    title = models.CharField(max_length=50, unique=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='img/blog', null=True)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    topped = models.BooleanField(default=False)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=False)
     tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title[:20]
@@ -62,8 +78,8 @@ class Comment(Base):
     url = models.URLField(blank=True)
     text = models.TextField()
 
-    article = models.ForeignKey(Article)
-    
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.text[:20]
 
